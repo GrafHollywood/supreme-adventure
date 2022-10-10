7d6fbbafbe617a29c1b294e6d69bf3b50c7cb4a3
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-10-11 00:03:14 +0300

    connect ui lib

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,5 +1,6 @@
 import styles from './app.module.scss';
 import { Todo } from '@supreme-adventure/data';
+import { Ui, Todos } from '@supreme-adventure/ui';
 import { useEffect, useState } from 'react';
 
 export function App() {
@@ -15,6 +16,8 @@ export function App() {
     <>
       <h1 className={styles['title']}>Hi, mom!</h1>
       <div>{obj ? obj.title : 'Loading'}</div>
+      <Ui />
+      <Todos />
     </>
   );
 }
