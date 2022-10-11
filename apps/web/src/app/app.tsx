d759ef226ab8e931fc7ddd3806cd353a82cc3dc6
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-10-11 22:38:05 +0300

    setup eslint import/export sorting

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,16 +1,11 @@
+import { Todos } from '@supreme-adventure/ui';
+
 import styles from './app.module.scss';
-import { Todo } from '@supreme-adventure/data';
-import { Ui, Todos } from '@supreme-adventure/ui';
-import { useEffect, useState } from 'react';
 
 export function App() {
-  const [obj, setObj] = useState<Todo | null>(null);
-
   return (
     <>
       <h1 className={styles['title']}>Hi, mom!</h1>
-      <div>{obj ? obj.title : 'Loading'}</div>
-      <Ui />
       <Todos />
     </>
   );
