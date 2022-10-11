82ecc45ca7c8066d22e784eda9bc22a5da4df548
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-10-12 00:22:25 +0300

    create user service

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://45.141.101.238:27017/supreme-adventure'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
