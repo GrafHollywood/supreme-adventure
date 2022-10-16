e3e21ff53b36dcd3c69bd6056a4a5f5698b13c68
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-10-16 23:35:06 +0300

    add user dto in user service

--- apps/api/src/app/users/users.service.ts
+++ apps/api/src/app/users/users.service.ts
@@ -4,30 +4,50 @@ import { Model } from 'mongoose';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserDto } from './dto/user.dto';
 import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  private convertUser = (user: UserDocument): UserDto => ({
+    id: user.id,
+    username: user.username,
+    passwordHash: user.passwordHash,
+    name: user.name,
+  });
+
+  async create(createUserDto: CreateUserDto): Promise<UserDto> {
     const createdUser = new this.userModel(createUserDto);
-    return createdUser.save();
+    const user = await createdUser.save();
+    return this.convertUser(user);
   }
 
-  async findAll(): Promise<User[]> {
-    return this.userModel.find().exec();
+  async findAll(): Promise<UserDto[]> {
+    const users = await this.userModel.find().exec();
+    return users.map((user) => this.convertUser(user));
   }
 
-  async findOne(id: string): Promise<User> {
-    return this.userModel.findById(id).exec();
+  async findOne(id: string): Promise<UserDto> {
+    const user = await this.userModel.findById(id).exec();
+    return this.convertUser(user);
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
-    return this.userModel.findByIdAndUpdate(id, updateUserDto).exec();
+  async findOneByUsername(username: string): Promise<UserDto> {
+    const user = await this.userModel.findOne({ username }).exec();
+    return this.convertUser(user);
   }
 
-  async remove(id: string): Promise<User> {
-    return this.userModel.findByIdAndRemove(id).exec();
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<UserDto> {
+    const user = await this.userModel
+      .findByIdAndUpdate(id, updateUserDto)
+      .exec();
+    return this.convertUser(user);
+  }
+
+  async remove(id: string): Promise<UserDto> {
+    const user = await this.userModel.findByIdAndRemove(id).exec();
+    return this.convertUser(user);
   }
 }
